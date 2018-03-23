de9bc1d9aa1c112d2c1c4b144a74c51a6b0b112f
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-03-23 01:09:29 -0700

    first train demo

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const brain = require("brain.js");
 const network = new brain.NeuralNetwork();
 
-network.train();
+network.train([
+  {input: { r, g, b }, output: {dark: 1}},
+]);
 
-const result = network.run()
+const result = network.run({height: 70, weight: 200});
+console.log(result);
